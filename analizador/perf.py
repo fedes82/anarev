92693cbcad4b2e239eaf91d72e750b4648ad2be6
federico <fedes82@gmail.com>
2018-08-21 01:35:32 -0300

    crear arbol
    crear arbol y algunos conetct

--- analizador/perf.py
+++ analizador/perf.py
@@ -8,6 +8,7 @@ from PyQt5.QtCore import Qt
 from PyQt5 import QtGui, QtWidgets
 
 import sys
+import json
 
 import perfiles5
 
@@ -17,14 +18,58 @@ class ProfilesWindow(QtWidgets.QMainWindow, perfiles5.Ui_Dialog):
         super().__init__()
        # self.setupUi()
     #connects
+        self.btnDelCat.clicked.connect(self.btn_DelCat)
+        self.btnImpPerfil.clicked.connect(self.btn_ImpPerfil)
+        self.btnAddParam.clicked.connect(self.btn_AddParam)
+        self.btnDelParam.clicked.connect(self.btn_DelParam)
+        self.btnAddCat.clicked.connect(self.btn_AddCat)
+        self.btnNuevoPerfil.clicked.connect(self.btn_NuevoPerfil)
+        self.btnExpPerfil.clicked.connect(self.btn_ExpPerfil)
     
+    #cargo el default
+        self.cargar_diccionario('perfiles/default.json')
+        self.actualizar_todo()
+        self.lblPerfil.setObjectName("lblPerfil")
+        self.lnNombreParam = QtWidgets.QLineEdit(self.groupBox)
+        self.lnCodeParam = QtWidgets.QLineEdit(self.groupBox)
+        self.lnCategorias
+        self.lstCategorias = QtWidgets.QListView(self.groupBox_2)
+        self.combPerfil = QtWidgets.QComboBox(Dialog)
+        
+#        self.buttonBox.accepted.connect(Dialog.accept)
+#        self.buttonBox.rejected.connect(Dialog.reject)
     
-    
+    def btn_AddParam(self):
+        parametro = str(self.lstParametros.currentItem().text()[0])
+        nombre = self.lstParametros.currentItem().text()
+        categoria = str(self.combValorParam.currentText())
+        
+        
+    def cargar_diccionario(self,archivo):
+        with open('archivo','r') as file:
+            self.dict_parametros = json.loads(file.read())
+        
     def cargar_parametros(self):
         self.lstParametros.clear()
         for key in dict_parametros:
             self.lstParametros.addItem(key)
+    
+    def crear_Arbol(self,dictionary):
+        arbol=''
+        for key in dictionary:
+            arbol += key +' - Cod: '+'\n'#, dictionary[key][0] +'\n'
+            for item in dictionary[key][1]:
+                arbol += '\t-- ' + item + '\n'
+        print(arbol)
+        return arbol
         
+    def actualizar_arbol(self):
+        self.arbol = crear_Arbol(self.dict_parametros)
+        self.lblPerfil.setText(self.arbol)
+        
+    def actualizar_todo(self):
+        self.cargar_parametros()
+        self.actualizar_arbol()
 def main():
    # global app
     app = QtWidgets.QApplication(sys.argv)
